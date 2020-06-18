3e2e59fed705c5a55ee2469c3442fddbbf87cab8
Dimas Miftahul Huda <dimas.mfth@gmail.com>
2020-06-18 22:05:55 +0700

    udah di-build

---
+++ dist/precache-manifest.bca427ce0c34be8c9bfed87c0121077f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0a04c273936d7b62306ada7ded85cfa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "423b6b05fc2d97f02047",
+    "url": "/static/css/main.e77cb324.chunk.css"
+  },
+  {
+    "revision": "04d44b44f8d0661c7f6f",
+    "url": "/static/js/2.69bb2aac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.69bb2aac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "423b6b05fc2d97f02047",
+    "url": "/static/js/main.2e70341c.chunk.js"
+  },
+  {
+    "revision": "85d1d2968c5402ca8210",
+    "url": "/static/js/runtime-main.a19f495d.js"
+  }
+]);
